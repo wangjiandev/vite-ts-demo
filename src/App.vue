435d0dd3b564617b238d6551ac4bbfc147640d67
wangjian <wangjian0504@gmail.com>
2022-08-30 15:14:16 +0800

    基础演练环境

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  // type MyPick<T, K extends keyof T> = {
+  //   [P in K]: T[P]
+  // }
+
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  type A = Pick<Todo, 'title' | 'completed'>
+
+  const a: A = {
+    title: 'dsdad',
+    completed: false
+  }
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    {{ a }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
